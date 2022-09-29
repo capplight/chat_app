c793bf2ebe315df402ac96d1c2634ceaaa140417
Nauryzbek Yechshanov <nauryzbek.ali@gmail.com>
2022-09-30 00:39:26 +0500

    This is a new commit for what I originally planned to be amended

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="9d19b2fa-eb77-4761-9359-74f7827d19b0"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+export default App;
